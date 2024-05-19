8466e79ed954cecf90962ae8f0f66d4530ab9dee
juanjosemas <48967497+juanjosemas@users.noreply.github.com>
2024-05-19 10:59:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -294,3 +294,25 @@ createCalendar();
 function updateCalendar(year, month) {
 // Tu código para actualizar el calendario iría aquí
 }
+
+
+
+const $tiempo=document.querySelector('.tiempo'),
+$fecha= document.querySelector('.fecha');
+
+function Relojdigital(){
+    let f=new Date(),
+    dia= f.getDate(),
+    mes= f.getMonth()+1,
+    anio= f.getFullYear(),
+    diaSemana=f.getDay();
+
+    dia= ('0'+dia).slice(-2);
+    mes=('0'+mes).slice(-2)
+
+    let timeString= f.toLocaleTimeString();
+    $tiempo.innerHTML=timeString;
+}
+setInterval(() =>{
+    Relojdigital()
+},100);
